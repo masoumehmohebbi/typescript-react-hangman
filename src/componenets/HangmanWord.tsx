1a0552ab478a9defef6588092a10dd553ad65c25
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-10-02 17:58:00 +0330

    Set type: activeKey: string[]     isWinner:boolean in HangmanWord.tsx

--- src/componenets/HangmanWord.tsx
+++ src/componenets/HangmanWord.tsx
@@ -3,24 +3,33 @@ type HangmanWordProps = {
     wordToGuess : string
     guessedLetters: string[]
     isLoser:boolean
+    activeKey: string[]
+    isWinner:boolean
   }
   
-  export default function HangmanWord({wordToGuess,guessedLetters,isLoser}: HangmanWordProps){
-    console.log(guessedLetters);
-    console.log(wordToGuess);
+  export default function HangmanWord({wordToGuess,guessedLetters,isLoser,isWinner,activeKey}: HangmanWordProps){
+
+    console.log(activeKey);
     
     
     return (
   <>
-  {isLoser ?
+  {isLoser &&
   <h1 className="uppercase text-center md:text-2xl text-slate-800 mt-2 mb-11">nice try, refresh to try agian! 👾</h1>
-   : <h1>good luck h,ey</h1>}
+}
+{isWinner &&
+  <h1 className="uppercase text-center md:text-2xl text-slate-800 mt-2 mb-11">Winner!!, refresh to try agian! 👾</h1>
+}
+{!isLoser && !isWinner && <h1 className="mx-auto text-black capitalize font-xl">hey, wish u good luck!</h1>}
   
       <div className="mx-auto mt-6">
   
         {wordToGuess.split("").map((w, index) => (
          <span key={index}  className="border-b-2 border-slate-600 mx-3 px-1">
-           <span className={`uppercase text-3xl font-black ${guessedLetters.includes(w) ? "visible" : "invisible"
+
+           <span className={`uppercase text-3xl font-black 
+           ${isLoser && !guessedLetters.includes(w) && 'text-red-500'}
+           ${guessedLetters.includes(w) || isLoser ? "visible" : "invisible"
           }`} >
             {w}
           </span>
