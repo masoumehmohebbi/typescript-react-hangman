34e5228e383b07dae15e103367352e1959e017f1
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-10-02 11:59:04 +0330

    Added incorrectLetters && Loser: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Keyboard from "./componenets/Keyboard"
 import word from "../data/wordList.json"
 import { useState } from "react";
+import HangmanDrawing from "./componenets/HangmanDrawing";
+import HangmanWord from "./componenets/HangmanWord";
 
 
 function App() {
@@ -8,14 +10,20 @@ function App() {
   const [wordToGuess, setWordToGeuss] = useState(word[Math.floor(Math.random()* word.length)])
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
  
+  const incorrectLetters = guessedLetters.filter((key)=>!wordToGuess.includes(key))
+  const isLoser = incorrectLetters.length > 5
+  
   return (
     <section className="grid grid-cols-12 gap-5 font-Gloria">
     <div className="col-span-12 md:col-span-5">
-    <HangmanDrawing/>
+    <HangmanDrawing 
+    numOFIncorrectLetters = {incorrectLetters.length}/>
     </div>
     <div className="flex flex-col col-span-12 md:col-span-7">
-      <HangmanWord wordToGuess={wordToGuess} guessedLetters={guessedLetters}/>
+      <HangmanWord isLoser={isLoser} wordToGuess={wordToGuess} guessedLetters={guessedLetters}/>
+
       <Keyboard
+        isLoser = {isLoser}
         activeKey ={guessedLetters.filter((key)=>wordToGuess.includes(key))}
         UnactiveKey ={guessedLetters.filter((key)=>!wordToGuess.includes(key))}
         setGuessedLetters={setGuessedLetters}/>
@@ -26,44 +34,3 @@ function App() {
 
 export default App
 
-function HangmanDrawing() {
-  return <section className="mt-9">
-    <div className="relative w-[280px] mx-auto h-52 md:h-96 flex justify-center items-center">
-      <div className="absolute bg-yellow-900 h-11 w-2 top-0 left-[94%] md:left-[calc(100%_+_3rem)]"></div>
-      <div className="absolute h-2 w-32 md:w-48 left-[140px] bg-yellow-900 top-0"></div>
-      <div className="h-full md:h-full bg-yellow-900 w-2 absolute bottom-0 left-[140px]"></div>
-      <div className="w-4/5 md:w-full h-2 bg-yellow-900 absolute bottom-0"></div>
-    </div>
-  </section>
-}
-type HangmanWordProps = {
-  wordToGuess : string,
-  guessedLetters: string[]
-}
-
-
-function HangmanWord({wordToGuess,guessedLetters}: HangmanWordProps){
-  console.log(guessedLetters);
-  console.log(wordToGuess);
-  
-  
-  return (
-<>
-<h1 className="uppercase text-center md:text-2xl text-slate-800 mt-2 mb-11">nice try, refresh to try agian! 👾</h1>
-
-    <div className="mx-auto mt-6">
-
-      {wordToGuess.split("").map((w, index) => (
-       <span key={index}  className="border-b-2 border-slate-600 mx-3 px-1">
-         <span className={`uppercase text-3xl font-black ${guessedLetters.includes(w) ? "visible" : "invisible"
-        }`} >
-          {w}
-        </span>
-       </span>
-
-      ))}
-    </div>
-</>
-  );
-  
-}
