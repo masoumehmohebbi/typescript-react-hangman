4c2b4808261df950dac14a3c4f0947b80aa20c21
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-10-02 19:53:46 +0330

    Removed activeKey-props: HangmanWord.tsx

--- src/componenets/HangmanWord.tsx
+++ src/componenets/HangmanWord.tsx
@@ -3,26 +3,22 @@ type HangmanWordProps = {
     wordToGuess : string
     guessedLetters: string[]
     isLoser:boolean
-    activeKey: string[]
     isWinner:boolean
   }
   
-  export default function HangmanWord({wordToGuess,guessedLetters,isLoser,isWinner,activeKey}: HangmanWordProps){
+  export default function HangmanWord({wordToGuess,guessedLetters,isLoser,isWinner}: HangmanWordProps){
 
-    console.log(activeKey);
-    
-    
     return (
   <>
   {isLoser &&
-  <h1 className="uppercase text-center md:text-2xl text-slate-800 mt-2 mb-11">nice try, refresh to try agian! 👾</h1>
+  <h1 className="uppercase text-center md:text-2xl text-slate-800 mt-11 md:mt-3 mb-11 ml-4 min-[400px]:ml-0">nice try, refresh to try agian! 😵</h1>
 }
 {isWinner &&
-  <h1 className="uppercase text-center md:text-2xl text-slate-800 mt-2 mb-11">Winner!!, refresh to try agian! 👾</h1>
+  <h1 className="uppercase text-center md:text-2xl text-slate-800 mt-11 md:mt-3 mb-11 ml-4 min-[400px]:ml-0">Winner!!, <a onClick={()=>window.location.reload()} className="text-orange-600 font-semibold" href="#">Click here</a> to try agian! 🎉</h1>
 }
-{!isLoser && !isWinner && <h1 className="mx-auto text-black capitalize font-xl">hey, wish u good luck!</h1>}
+{!isLoser && !isWinner && <h1 className="uppercase text-center md:text-2xl text-slate-800 mt-11 md:mt-3 mb-11 ml-4 min-[400px]:ml-0">hey, wish u good luck! 👾</h1>}
   
-      <div className="mx-auto mt-6">
+      <div className="mx-auto mt-12">
   
         {wordToGuess.split("").map((w, index) => (
          <span key={index}  className="border-b-2 border-slate-600 mx-3 px-1">
