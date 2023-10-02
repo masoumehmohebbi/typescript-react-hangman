e9c21780589de098d2846075d1d7b638f475b4f0
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-10-02 17:57:14 +0330

    Added isWinner: App.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,15 @@ function App() {
   // Choose a random word of wordList.json
   const [wordToGuess, setWordToGeuss] = useState(word[Math.floor(Math.random()* word.length)])
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
- 
-  const incorrectLetters = guessedLetters.filter((key)=>!wordToGuess.includes(key))
+
+
+  const incorrectLetters = guessedLetters.filter((letter)=> !wordToGuess.includes(letter))
+  
+  
+
+
   const isLoser = incorrectLetters.length > 5
+  const isWinner = wordToGuess.split('').every((letter)=> guessedLetters.includes(letter))
   
   return (
     <section className="grid grid-cols-12 gap-5 font-Gloria">
@@ -20,10 +26,16 @@ function App() {
     numOFIncorrectLetters = {incorrectLetters.length}/>
     </div>
     <div className="flex flex-col col-span-12 md:col-span-7">
-      <HangmanWord isLoser={isLoser} wordToGuess={wordToGuess} guessedLetters={guessedLetters}/>
+      <HangmanWord
+        activeKey ={guessedLetters.filter((key)=>wordToGuess.includes(key))}
+       isLoser={isLoser} wordToGuess={wordToGuess} guessedLetters={guessedLetters}
+       isWinner = {isWinner}
+       />
+      
 
       <Keyboard
-        isLoser = {isLoser}
+        isLoser = {isLoser}   
+        isWinner = {isWinner}
         activeKey ={guessedLetters.filter((key)=>wordToGuess.includes(key))}
         UnactiveKey ={guessedLetters.filter((key)=>!wordToGuess.includes(key))}
         setGuessedLetters={setGuessedLetters}/>
