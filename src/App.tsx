cdb1043b0b0082943a8e8050ac5f0244a10ab04b
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-10-02 19:52:45 +0330

    Added KeyPress-Event For Keys: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,47 @@
 import Keyboard from "./componenets/Keyboard"
 import word from "../data/wordList.json"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import HangmanDrawing from "./componenets/HangmanDrawing";
 import HangmanWord from "./componenets/HangmanWord";
 
 
 function App() {
   // Choose a random word of wordList.json
-  const [wordToGuess, setWordToGeuss] = useState(word[Math.floor(Math.random()* word.length)])
+  const [wordToGuess] = useState(word[Math.floor(Math.random()* word.length)])
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
-
-
   const incorrectLetters = guessedLetters.filter((letter)=> !wordToGuess.includes(letter))
-  
-  
-
-
   const isLoser = incorrectLetters.length > 5
   const isWinner = wordToGuess.split('').every((letter)=> guessedLetters.includes(letter))
+
   
+  useEffect(()=>{
+    const handleKeys = (e:KeyboardEvent) =>{
+        if (!e.key.match(/^[a-z]$/)) return
+        if (guessedLetters.includes(e.key) || isLoser || isWinner) return
+        setGuessedLetters((prev)=>[...prev,e.key])    
+    }
+
+    document.addEventListener('keypress', handleKeys)
+
+    return () => document.removeEventListener('keypress', handleKeys)
+  },[guessedLetters,isLoser,isWinner])
+
+
   return (
     <section className="grid grid-cols-12 gap-5 font-Gloria">
     <div className="col-span-12 md:col-span-5">
+      
     <HangmanDrawing 
     numOFIncorrectLetters = {incorrectLetters.length}/>
     </div>
+
     <div className="flex flex-col col-span-12 md:col-span-7">
+
       <HangmanWord
-        activeKey ={guessedLetters.filter((key)=>wordToGuess.includes(key))}
        isLoser={isLoser} wordToGuess={wordToGuess} guessedLetters={guessedLetters}
        isWinner = {isWinner}
        />
-      
-
+  
       <Keyboard
         isLoser = {isLoser}   
         isWinner = {isWinner}
